2f7200014f3ff157f4d9640f94e727deefad8110
Jerry Li <jerli@xogrp.com>
2021-09-04 16:51:39 +0800

    Learne function basics.

---
+++ bin/chap5/function_basics.dart
@@ -0,0 +1,44 @@
+void main(List<String> args) {
+  // isInRange(2);
+  isInRangeSecond(2, minimum: 2, maximum: 44);
+}
+
+//Making parameters optionl
+String generateFullname(String firstName, String lastName, [String? title]) {
+  final fullName = '$firstName $lastName';
+  var result = fullName;
+  if (title != null) {
+    result = '';
+  }
+  return result;
+}
+
+//Providing default value
+
+bool isInRange(int value, [int minimum = 0, int maximum = 10]) {
+  return minimum <= value && maximum >= value;
+}
+
+//Naming paraemters
+bool isInRangeSecond(int value, {int minimum = 0, int maximum = 10}) {
+  return minimum <= value && maximum >= value;
+}
+
+//Makeing named parameters required
+bool isInRangeThird({
+  required int value,
+  int minimum = 0,
+  int maximum = 10,
+}) {
+  return minimum <= value && maximum >= value;
+}
+
+//Optional type
+
+String compliement(String name) {
+  return '$name is a very nice person.';
+}
+
+complimentWithOptionalType(name) {
+  return '$name is a very nice persion';
+} 
